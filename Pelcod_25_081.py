e7cf5a21ed9700d72a3a976edf6a2a600768e4e6
Anggaror <nanggalayudhapratama81@gmail.com>
2025-09-18 13:51:01 +0700

    Add files via upload

---
+++ Pelcod_25_081.py
@@ -0,0 +1,17 @@
+nama = "Nanggala Yuda Pratama"
+umur = 18
+tinggi = 171.5  # cm
+angka_keberuntungan = 26
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi, "cm")
+print("Angka Favorit:", angka_keberuntungan)
+
+total_harga = (4 * 2000) + (2 * 5000)
+print("Total harga belanja:", total_harga)
+
+if angka_keberuntungan % 2 == 0:
+    print("angka genap")
+else:
+    print("angka ganjil")
